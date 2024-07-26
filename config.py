2241296fa1243d3b885e6365d64bd0c80517a403
Priyadharshini <108666828+priya036@users.noreply.github.com>
2024-07-26 23:14:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 UPLOAD_FOLDER = 'static/uploads/'
 DB_NAME = 'criminal_detection_data'
-MONGO_URI = 'mongodb+srv://priyadharshinis264:POdAdRlYqrRdN7gq@criminaldata.fz2vp0c.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URI = 'mongodb+srv://priyadharshinis264:POdAdRlYqrRdN7gq@criminaldata.fz2vp0c.mongodb.net/'
 SECRET_KEY = os.urandom(24)
