fdf17520f629236a7d25ddde8fdf63298e82d60c
Priyadharshini <108666828+priya036@users.noreply.github.com>
2024-08-05 21:45:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 UPLOAD_FOLDER = 'static/uploads/'
 DB_NAME = 'criminal_detection_data'
-MONGO_URI = 'mongodb+srv://priya:5799@criminaldata.jhclr9m.mongodb.net/'
+MONGO_URI = 'mongodb+srv://priya:5799@criminaldata.jhclr9m.mongodb.net/?retryWrites=true&w=majority&appName=CriminalData'
 SECRET_KEY = os.urandom(24)
